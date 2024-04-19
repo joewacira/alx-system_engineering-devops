919d4609c85c3022ec8e0418ddb50957fec0ba46
joewacira <josephwmuthon@gmail.com>
2024-04-19 16:30:08 -0700

    puppet manifest that creates a file school in /tmp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# A puppet manifest that creates a file school in /tmp.
+
+file { '/tmp/school':
+	ensure  => present,
+	mode    => '0744',
+	owner   => 'www-data',
+	group   => 'www-data',
+	content => 'I love Puppet',
+}
